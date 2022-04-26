c4a9c78f49cbd3496f9e72239a21ef932c8d3601
Junior Dourado <juniordourado@MacBook-Pro-de-Junior.local>
2022-04-25 21:27:48 -0300

    first commit

---
+++ microservico-documento-kotlin/sgd/src/main/kotlin/com/sgd/SgdApplication.kt
@@ -0,0 +1,11 @@
+package com.sgd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SgdApplication
+
+fun main(args: Array<String>) {
+	runApplication<SgdApplication>(*args)
+}
